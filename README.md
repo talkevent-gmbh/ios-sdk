01c8dd974ffb71538540f2c1ede3424178010d64
Jan Bürling <jan.buerling@ambient-innovation.com>
2016-11-11 15:47:33 +0100

    Add files to repository

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ios-sdk
+# TalkEventSDK
+
+## Installation
+
+### Pods
+Das Framework wurde bei Cocoapods(https://cocoapods.org/) hochgeladen und dient zur vereinfachten Installation.
+Wie Sie Ihr Projekt mit Cococapods initialisieren entnehmen Sie bitte unter folgendem Link: https://guides.cocoapods.org/using/using-cocoapods.html
+
+Anschließend das Framework mit pod 'TalkEventSDK' zu Ihrer Podfile hinzufügen
+
+## Anbindung 
+
---
+++ TalkEventSDK.framework/Headers/TalkEventSDK.h
@@ -0,0 +1,19 @@
+//
+//  TalkEventSDK.h
+//  TalkEventSDK
+//
+//  Created by Jan Bürling on 04.11.16.
+//  Copyright © 2016 TalkEvent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TalkEventSDK.
+FOUNDATION_EXPORT double TalkEventSDKVersionNumber;
+
+//! Project version string for TalkEventSDK.
+FOUNDATION_EXPORT const unsigned char TalkEventSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TalkEventSDK/PublicHeader.h>
+
+
